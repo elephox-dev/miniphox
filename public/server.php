49ca4fe9846736483acca2d334510fd28cccf6a0
Ricardo Boss <contact@ricardoboss.de>
2022-10-17 16:44:45 +0200

    Initial commit

---
+++ public/server.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
+use Elephox\Miniphox\Attributes\Get;
+use Elephox\Miniphox\Miniphox;
+
+#[Get] // defaults to '/'
+function index(): string {
+	return "Hello, World!";
+}
+
+#[Get('/greet/[name]')] // using route params
+function greet(string $name): string {
+	return "Hello, $name!";
+}
+
+// This creates a new Miniphox app.
+Miniphox::build()
+
+	// index() and greet() are mounted at '/api'.
+	// This maps '/api' -> index() and '/api/greet/[name]' -> greet() according to their attributes above.
+	//
+	// You can pass first-class-callables or just the method name to the mount method.
+	->mount('/api', [index(...), 'greet'])
+
+	// This will start the HTTP server on http://0.0.0.0:8008.
+	// Pass a string uri to bind to a specific ip address or a different port.
+	->run();
